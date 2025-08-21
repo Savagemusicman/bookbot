a7cbe55e6369b4dea4096d569405dbd880498c03
Savagemusicman <dwinterz89@gmail.com>
2025-08-21 15:27:57 -0500

    added change to sys.arg usage

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from stats import (
 def main():
 
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <book_path>")
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
 
